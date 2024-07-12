05924e346f10d1debeb83bf872e79abcc3468bae
Christine Tina Park <escp1004jk@gmail.com>
2024-07-12 13:00:05 +0200

    Changed WC to staging and configured WC verify button. - TP

--- packages/next-app/app/page.tsx
+++ packages/next-app/app/page.tsx
@@ -1,10 +1,12 @@
 import ConnectButton from './components/connectbutton';
+import VerifyButton from './components/verifybutton';
 
 export default function Home() {
     return (
         <main>
             <h1>Home</h1>
             <ConnectButton />
+            <VerifyButton />
         </main>
     );
 }
