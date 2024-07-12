6f596bc50b874dc153cb342a699438c6428c3617
Christine Tina Park <escp1004jk@gmail.com>
2024-07-12 16:44:09 +0200

    Upon connect and verify, add values to AppContext, accessible throughout the app. - TP

--- packages/next-app/app/components/verifybutton.tsx
+++ packages/next-app/app/components/verifybutton.tsx
@@ -1,13 +1,26 @@
 'use client'; // for Next.js app router
 import { IDKitWidget, VerificationLevel, ISuccessResult } from '@worldcoin/idkit';
+import { useVerification } from '@/context/AppContext';
 
 export default function VerifyButton() {
+    const { verifyMessage, setVerifyMessage } = useVerification();
+    const handleVerify = (message: {
+        merkle_root: string;
+        nullifier_hash: string;
+        proof: string;
+        verification_level: string;
+    }) => {
+        setVerifyMessage(message);
+        console.log(`message: ${message}`);
+        console.log(`State verifyMessage: ${verifyMessage}`)
+    };
+
     return (
         <IDKitWidget
             app_id={`app_staging_${process.env.NEXT_PUBLIC_WC_ACTION_APP_ID}`}
             action={process.env.NEXT_PUBLIC_WC_ACTION_NAME || ''}
             onSuccess={message => console.log(message)}
-            handleVerify={message => console.log(message)} // callback when the proof is received
+            handleVerify={handleVerify}
             verification_level={VerificationLevel.Orb} //VerificationLevel.Device?
         >
             {({ open }) => <button onClick={open}>Verify with World ID</button>}
