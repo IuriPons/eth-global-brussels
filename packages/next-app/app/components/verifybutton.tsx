6887b30accdb14e0ae039bfb226437d9ae1066eb
Christine Tina Park <escp1004jk@gmail.com>
2024-07-12 15:37:52 +0200

    Added next config for WalletConnect and updated buttons. - TP

--- packages/next-app/app/components/verifybutton.tsx
+++ packages/next-app/app/components/verifybutton.tsx
@@ -4,7 +4,7 @@ import { IDKitWidget, VerificationLevel, ISuccessResult } from '@worldcoin/idkit
 export default function VerifyButton() {
     return (
         <IDKitWidget
-            app_id={process.env.NEXT_PUBLIC_WC_ACTION_APP_ID}
+            app_id={`app_staging_${process.env.NEXT_PUBLIC_WC_ACTION_APP_ID}`}
             action={process.env.NEXT_PUBLIC_WC_ACTION_NAME || ''}
             onSuccess={message => console.log(message)}
             handleVerify={message => console.log(message)} // callback when the proof is received
