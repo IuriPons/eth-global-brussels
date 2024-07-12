c539faf05004ba338e5c7fd82cf731bac496cc97
josepchetrit12 <jchetrit1999@gmail.com>
2024-07-12 16:01:01 +0200

    create scripts to deploy uni v4 and hooks

---
+++ packages/foundry/script/00_Hook.s.sol
@@ -0,0 +1,37 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+import "forge-std/Script.sol";
+import {Vm} from "forge-std/Test.sol";
+
+import {Hooks} from "v4-core/libraries/Hooks.sol";
+import {IPoolManager} from "v4-core/interfaces/IPoolManager.sol";
+
+import {HookMiner} from "../script/utils/HookMiner.sol";
+import {Hook} from "../src/Hook.sol";
+import "forge-std/console.sol";
+
+contract HookScript is Script {
+    address constant CREATE2_DEPLOYER = address(0x4e59b44847b379578588920cA78FbF26c0B4956C);
+    address constant GOERLI_POOLMANAGER = address(0x3A9D48AB9751398BbFa63ad67599Bb04e4BdF98b);
+
+    function setUp() public {}
+
+    function run() public {
+        // hook contracts must have specific flags encoded in the address
+        uint160 flags = uint160(
+            Hooks.BEFORE_SWAP_FLAG 
+        );
+
+        // Mine a salt that will produce a hook address with the correct flags
+        (address hookAddress, bytes32 salt) =
+            HookMiner.find(CREATE2_DEPLOYER, flags, type(Hook).creationCode, abi.encode(address(GOERLI_POOLMANAGER)));
+
+        // Deploy the hook using CREATE2
+        vm.broadcast();
+        Hook hook = new Hook{salt: salt}(IPoolManager(address(GOERLI_POOLMANAGER)));
+        require(address(hook) == hookAddress, "CounterScript: hook address mismatch");
+
+        console.log("Hook deployed in", hookAddress);
+    }
+}
---
+++ packages/foundry/src/Hook.sol
@@ -0,0 +1,70 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import {BaseHook} from "v4-periphery/BaseHook.sol";
+
+import {CurrencyLibrary, Currency} from "v4-core/types/Currency.sol";
+import {PoolKey} from "v4-core/types/PoolKey.sol";
+import {BalanceDeltaLibrary, BalanceDelta} from "v4-core/types/BalanceDelta.sol";
+
+import {IPoolManager} from "v4-core/interfaces/IPoolManager.sol";
+
+import {Hooks} from "v4-core/libraries/Hooks.sol";
+
+contract Hook is BaseHook {
+    using CurrencyLibrary for Currency;
+    using BalanceDeltaLibrary for BalanceDelta;
+
+    constructor(
+        IPoolManager _manager
+    ) BaseHook(_manager) {}
+
+    function getHookPermissions()
+        public
+        pure
+        override
+        returns (Hooks.Permissions memory)
+    {
+        return
+            Hooks.Permissions({
+                beforeInitialize: false,
+                afterInitialize: false,
+                beforeAddLiquidity: false,
+                beforeRemoveLiquidity: false,
+                afterAddLiquidity: false,
+                afterRemoveLiquidity: false,
+                beforeSwap: true,
+                afterSwap: false,
+                beforeDonate: false,
+                afterDonate: false,
+                beforeSwapReturnDelta: false,
+                afterSwapReturnDelta: false,
+                afterAddLiquidityReturnDelta: false,
+                afterRemoveLiquidityReturnDelta: false
+            });
+    }
+
+    function afterSwap(
+        address,
+        PoolKey calldata key,
+        IPoolManager.SwapParams calldata swapParams,
+        BalanceDelta delta,
+        bytes calldata hookData
+    ) external override onlyByManager returns (bytes4, int128) {
+
+        return (this.afterSwap.selector, 0);
+    }
+
+    function afterAddLiquidity(
+        address,
+        PoolKey calldata key,
+        IPoolManager.ModifyLiquidityParams calldata,
+        BalanceDelta delta,
+        bytes calldata hookData
+    ) external override onlyByManager returns (bytes4, BalanceDelta) {
+
+        return (this.afterAddLiquidity.selector, delta);
+    }
+
+    
+}
