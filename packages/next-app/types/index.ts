b0acda3f184e0bb1fb3d1cb81f07589e0b849172
Iuri Pons <iuri.19.05@gmail.com>
2024-07-12 20:37:33 +0200

    Format Code

--- packages/next-app/types/index.ts
+++ packages/next-app/types/index.ts
@@ -1,20 +1,19 @@
-
 export type SwapInfo = {
     sellAmount?: number;
     sellCoin: Coin;
     buyAmount?: number;
     buyCoin: Coin;
-}
+};
 
 export type Coin = {
     name: string;
     symbol: string;
     icon: string;
-}
+};
 
 export type PoolCreationInfo = {
     coin1: Coin;
     coin2: Coin;
     fee: number;
     hook: string;
-}
+};
