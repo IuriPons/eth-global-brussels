9c0b1cc6a845af02020ead975fff563aa9223ab5
Christine Tina Park <escp1004jk@gmail.com>
2024-07-12 22:02:43 +0200

    nav bar update

--- packages/next-app/components/ui/Navbar.tsx
+++ packages/next-app/components/ui/Navbar.tsx
@@ -1,15 +1,11 @@
 'use client';
 
-import ConnectButton from '@/components/ui/ConnectButton';
-import VerifyButton from '@/components/ui/VerifyButton';
-import { useConnectedAccount, useVerification } from '@/context/AppContext';
-import { AppBar, Box, Button, Toolbar } from '@mui/material';
-
-// Navigation Links
-const navLinks = [
-    { title: 'Swap', path: '/' },
-    { title: 'Create Pool', path: '/create-pool' },
-];
+import ConnectButton from '@/app/components/connectbutton';
+import VerifyButton from '@/app/components/verifybutton';
+import { AppBar, Toolbar, Typography } from '@mui/material';
+import { useVerification, useConnectedAccount } from '@/context/AppContext';
+import Image from 'next/image';
+import Link from 'next/link';
 
 export default function Navbar() {
     const { verifyMessage } = useVerification();
@@ -19,17 +15,20 @@ export default function Navbar() {
         <>
             <AppBar position='static'>
                 <Toolbar sx={{ justifyContent: 'space-between', alignItems: 'center' }}>
-                    <Box sx={{ gap: 2 }}>
-                        {/* <Typography variant='h6' component='a' href='/'>
-                            My App
-                        </Typography> */}
-                        {navLinks.map(link => (
-                            <Button key={link.title} color='inherit' component='a' href={link.path}>
-                                {link.title}
-                            </Button>
-                        ))}
-                    </Box>
-
+                    <div className='flex items-center space-x-4'>
+                        <Link href='/' passHref>
+                            <Image src='/applogo.gif' alt='App Logo' width={200} height={100} />
+                        </Link>
+                        <Link href='/create' passHref>
+                            <button>CREATE</button>
+                        </Link>
+                        <Link href='/pools' passHref>
+                            <button>POOLS</button>
+                        </Link>
+                        <Link href='/swipe' passHref>
+                            <button>SWIPE</button>
+                        </Link>
+                    </div>
                     <div className='flex items-center space-x-4'>
                         <ConnectButton />
                         {connectedAccount.status === 'connected' && (
